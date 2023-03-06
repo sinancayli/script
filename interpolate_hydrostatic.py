ed1c70b86152a895dbf12366b581c63364bbcf43
sinancayli <55760676+sinancayli@users.noreply.github.com>
2023-03-06 03:53:05 +0300

    Add files via upload

---
+++ interpolate_hydrostatic.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+source_file = '/Users/scayli0/Desktop/ERNASU_HYDROSTATIC_899.xlsx'
+destination_file = '/Users/scayli0/Desktop/ERNASU_HYDROSTATIC_899_INTERPOLATED.xlsx'
+
+float_digit = '%.4f'
+
+# Read in data from source.xlsx file
+df = pd.read_excel(source_file)
+
+# Get the name of the first column
+column1_name = df.columns[0]
+
+# Interpolate data
+step = 0.001
+interpolated_data = pd.DataFrame({column1_name: np.arange(df[column1_name].min(), df[column1_name].max()+step, step)})
+for column in df.columns[1:]:
+    interpolated_data[column] = np.interp(interpolated_data[column1_name], df[column1_name], df[column])
+
+# Write interpolated data to file
+interpolated_data.to_excel(destination_file, index=False, float_format=float_digit, freeze_panes=(1, 1))
---
+++ interpolate_sounding.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+# Source and destination file paths
+source_file = '/Users/scayli0/Desktop/source_sounding.xlsx'
+destination_file = '/Users/scayli0/Desktop/destination_sounding.xlsx'
+
+# Float format
+float_digit = '%.3f'
+
+# Define the step size
+step = 0.01
+
+# Load the xls file into a pandas ExcelFile object
+xls = pd.ExcelFile(source_file)
+
+# Create a new ExcelWriter object for the destination file
+with pd.ExcelWriter(destination_file) as writer:
+    # Add an empty sheet named "CAL"
+    pd.DataFrame().to_excel(writer, sheet_name='CAL')
+
+    # Create a dataframe with the column names
+    column_names_df = pd.DataFrame({
+        'NAME': xls.sheet_names,
+        'DRAFT': 0.00,
+        'TRIM': 0.00,
+        'VOLUME': ''
+    })
+
+    # Write the column names dataframe to the CAL sheet
+    column_names_df.to_excel(writer, sheet_name='CAL', startrow=0, index=False, float_format=float_digit)
+
+    # Iterate over all sheets in the source Excel file
+    for sheet_name in xls.sheet_names:
+        # Load the sheet into a pandas DataFrame
+        df = pd.read_excel(xls, sheet_name=sheet_name)
+
+        # Set the index column to be the first column of the DataFrame
+        df = df.set_index(df.columns[0])
+
+        # Determine the starting and ending points based on the step size and the number of rows in the DataFrame
+        start = df.index.min()
+        end = df.index.max()
+        num_rows = round((end - start) / step) + 1
+
+        # Create a new DataFrame with the desired index values for interpolation
+        new_index = pd.Index([start + i * step for i in range(num_rows)], name=df.index.name)
+        new_df = pd.DataFrame(index=new_index)
+
+        # Merge the original DataFrame with the new DataFrame and interpolate missing values
+        interpolated_df = df.merge(new_df, how='outer', left_index=True, right_index=True).interpolate()
+
+        # Transpose the DataFrame
+        df_T = interpolated_df.T
+
+        # Determine the starting and ending points based on the step size and the number of rows in the DataFrame
+        start = df_T.index.min()
+        end = df_T.index.max()
+        num_rows = round((end - start) / step) + 1
+
+        # Create a new DataFrame with the desired index values for interpolation
+        new_index = pd.Index([start + i * step for i in range(num_rows)], name=df_T.index.name)
+        new_df = pd.DataFrame(index=new_index)
+
+        # Merge the original DataFrame with the new DataFrame and interpolate missing values
+        interpolated_df_T = df_T.merge(new_df, how='outer', left_index=True, right_index=True).interpolate()
+
+        # Transpose the result back to get the interpolated values for the rows
+        interpolated_df = interpolated_df_T.T
+
+        # Save the interpolated data to the corresponding sheet in the destination Excel file with the same sheet name
+        interpolated_df.to_excel(writer, sheet_name=sheet_name, freeze_panes=(1, 1), float_format=float_digit)
